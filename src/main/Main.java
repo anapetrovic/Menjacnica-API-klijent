80b83284c039493b98b89b8d19f04f030d5d7663
anapetrovic <anapetrovic994@gmail.com>
2015-05-23 21:27:46 +0200

    Napravljena klasa Main u paketu main.

---
+++ src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import menjacnica.AzuriranjeKursneListe;
+import menjacnica.Valuta;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AzuriranjeKursneListe a = new AzuriranjeKursneListe();
+		
+		LinkedList<Valuta> val = new LinkedList<Valuta>();
+		Valuta v = new Valuta();
+		v.setNaziv("EUR");
+		val.add(v);
+		Valuta v2 = new Valuta();
+		v2.setNaziv("USD");
+		val.add(v2);
+		
+		a.upisiValute(val, new GregorianCalendar());
+		
+		a.azurirajValute();
+
+	}
+
+}
+
